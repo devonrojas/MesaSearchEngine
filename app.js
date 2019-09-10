f242e5035d3876035da94445963a496db92f23a7
Devon Rojas <devonmrojas@gmail.com>
2019-09-10 15:47:20 -0700

    Academic Program Search API

--- app.js
+++ app.js
@@ -13,7 +13,41 @@ app.get("/", (req, res) => {
 })
 
 app.get("/search", (req, res) => {
-    res.status(200).send(SEARCH_DATA);
+    let query = req.query;
+    let searchTerm = query.q;
+    if(searchTerm) {
+        searchTerm = searchTerm.toLowerCase().replace(/\ /g, "");
+    }
+
+    if(searchTerm) {
+        let filteredData = {};
+        filteredData["programs"] = SEARCH_DATA["programs"]
+        .map(program => keywords(program))
+        .filter(program => program["search"].includes(searchTerm));
+        res.status(200).send(filteredData);
+    } else {
+        res.status(200).send(SEARCH_DATA);
+    }
 })
 
 app.listen(PORT, () => console.log(`App running on port: ${PORT}.`));
+
+const keywords = (program) => {
+    let str = [];
+    program["degree_types"].forEach(type => {
+        let s = type.split(" ");
+        str.push(...s);
+    })
+    program["degrees"].forEach(degree => {
+        let s = degree.replace(":", "").replace("-", "").split(" ");
+        str.push(...s);
+    })
+    program["keywords"].forEach(keyword => {
+        let s = keyword.split(" ");
+        str.push(...s);
+    })
+    str = [...new Set(str)].join("");
+
+    program["search"] = str.toLowerCase();
+    return program;
+}
